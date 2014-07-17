e6256dfdeac23faf119766dbb8b5129045f6fe7f
Bruno Bord <bruno@jehaisleprintemps.net>
2014-07-17 23:19:24 +0200

    First release v1.0.0

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup
 
-VERSION = '1.0-dev'
+VERSION = '1.0.0'
 
 if __name__ == '__main__':
     setup(
         name='sitedrifter',
+        url='https://github.com/brunobord/sitedrifter/',
+        author="Bruno Bord",
+        author_email='bruno@jehaisleprintemps.net',
+        license="MIT",
+        platforms='any',
         version=VERSION,
         install_requires=['Markdown', "Jinja2"],
         scripts=['bin/drift']
