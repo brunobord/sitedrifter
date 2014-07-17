3d3302773b37f4617e1c048a05a48e1d2b5cf912
Bruno Bord <bruno@jehaisleprintemps.net>
2014-07-17 23:39:56 +0200

    tidy up setup. ready for release

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 
 VERSION = '1.0.0'
 
-if __name__ == '__main__':
-    setup(
-        name='sitedrifter',
-        url='https://github.com/brunobord/sitedrifter/',
-        author="Bruno Bord",
-        author_email='bruno@jehaisleprintemps.net',
-        license="MIT",
-        platforms='any',
-        version=VERSION,
-        install_requires=['Markdown', "Jinja2"],
-        scripts=['bin/drift']
-    )
+setup(
+    name='sitedrifter',
+    description="Sitedrifter is a Markdown to HTML converter to build static websites.",
+    url='https://github.com/brunobord/sitedrifter/',
+    author="Bruno Bord",
+    author_email='bruno@jehaisleprintemps.net',
+    license="MIT",
+    platforms='any',
+    version=VERSION,
+    install_requires=['Markdown', "Jinja2"],
+    scripts=['bin/drift']
+)
