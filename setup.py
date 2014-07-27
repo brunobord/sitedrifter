eccf76c38438d0ac1a5d50935d587c1f68bbb889
Bruno Bord <bruno@jehaisleprintemps.net>
2014-07-27 22:33:14 +0200

    added long description -- close #2

--- setup.py
+++ setup.py
@@ -4,7 +4,50 @@ VERSION = '1.0.0-dev'
 
 setup(
     name='sitedrifter',
-    description="Sitedrifter is a Markdown to HTML converter to build static websites.",
+    description="Sitedrifter is a Markdown to HTML converter to build "
+                "static websites.",
+    long_description="""
+===========
+Sitedrifter
+===========
+
+Sitedrifter is a Markdown to HTML converter to build static websites. It's an
+enhanced version of the builder used in `Star drifter
+project <https://github.com/brunobord/stardrifter>`_, hence the name.
+
+Overview
+--------
+
+* Source files are in Markdown,
+* You can build templates using Jinja2 syntax,
+* Handling static files by copying `static` and `vendor` directories,
+* Navigation handling via a JSON file,
+* Fragment system: any snippet that has to be published more than once in the
+  website can be put into a fragment and duplicated into the content,
+* The ``drift`` binary tool has many options to customize source and build
+  targets.
+
+Install
+-------
+
+::
+
+    pip install sitedrifter
+
+Usage
+-----
+
+::
+
+    drift init  # To start a site project
+    drift build  # to build the website
+
+License
+-------
+
+This is a MIT-licensed software, by Bruno Bord (c) 2014.
+
+    """,
     url='https://github.com/brunobord/sitedrifter/',
     author="Bruno Bord",
     author_email='bruno@jehaisleprintemps.net',
